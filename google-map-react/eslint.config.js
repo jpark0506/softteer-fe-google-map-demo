bdd9f2904b501801765de633e0a4c94c46c20470
jpark0506 <peterp1@inha.edu>
2025-01-21 09:37:33 +0000

    Download & Apply Prettier

--- google-map-react/eslint.config.js
+++ google-map-react/eslint.config.js
@@ -1,12 +1,34 @@
 import globals from "globals";
+import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "no-console": "warn",
+      "eqeqeq": ["error", "always"],
+      "indent": "off",
+      "semi": "off",
+      "quotes": "off",
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+  eslintConfigPrettier,
 ];
